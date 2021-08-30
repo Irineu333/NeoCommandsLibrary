57f12e32f9a0df152dc8068f72c24b51a839dad0
Irineu A. Silva <mmmirineusilva@gmail.com>
2021-08-30 16:06:27 -0300

    Criando o apk builder test

--- app/src/main/java/neo/commands/library/MainActivity.kt
+++ app/src/main/java/neo/commands/library/MainActivity.kt
@@ -1,25 +1,38 @@
 package neo.commands.library
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import neo.commands.build.aapt.AaptUtils
 import neo.commands.build.dx.DxUtils
+import neo.commands.build.ecj.EcjUtils
+import neo.commands.library.databinding.ActivityMainBinding
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
         val commandDebug = "commandDebug"
         val native = applicationInfo.nativeLibraryDir
 
-        DxUtils("$native/dx.jar.so").help().apply {
-            if (isSuccess) {
-                Log.d(commandDebug, result!!)
-            } else {
-                Log.e(commandDebug, error!!)
-            }
-        }
+        //utils
+        val aapt = AaptUtils("$native/aapt-arm.so")
+        val ecj = EcjUtils("$native/ecj.jar.so")
+        val dc = DxUtils("$native/dx.jar.so")
+
+        val apkSigner = File("$native/apksigner.jar.so")
+
+
+//        binding.createR.setOnClickListener {
+//            aapt.syncCreateR()
+//        }
+
     }
 
 
